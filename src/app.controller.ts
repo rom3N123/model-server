3e28733f4d41492b0d9618ce558e2a476d084fd0
Roman Martynov <r.martynov@gk-adept.ru>
2023-09-11 09:58:03 +0300

    feat: make models get

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,27 +1,95 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post } from '@nestjs/common';
 import { AppService } from './app.service';
 import { FormDataRequest, MemoryStoredFile } from 'nestjs-form-data';
 import * as tf from '@tensorflow/tfjs-node-gpu';
 import { IMAGE_HEIGHT, IMAGE_WIDTH, model } from './model';
+import { mkdir, readFile, readdir, writeFile } from 'fs/promises';
+import { readFileSync } from 'fs';
 
 export type TrainDto = {
   modelName: string;
+  modelDescription: string;
   item1Name: string;
-  item1Images: MemoryStoredFile[];
   item2Name: string;
+  item1Images: MemoryStoredFile[];
   item2Images: MemoryStoredFile[];
-  epochs: number;
+  epochs: string;
+};
+
+export type StartDto = {
+  images: MemoryStoredFile[];
 };
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
+  @Post('models/:modelId/start')
+  @FormDataRequest()
+  async startModel(
+    @Param('modelId') modelId: string,
+    @Body() { images }: StartDto,
+  ) {
+    const modelPath = `./models/${modelId}`;
+    const model = await tf.loadLayersModel(`file://${modelPath}/model.json`);
+    const items = JSON.parse(
+      await readFile(`${modelPath}/items.json`, {
+        encoding: 'utf-8',
+      }),
+    );
+
+    const processedImages = images.map((file) => {
+      const decodedImage = tf.node.decodeImage(file.buffer);
+      const resizedImage = tf.image.resizeBilinear(decodedImage, [
+        IMAGE_WIDTH,
+        IMAGE_HEIGHT,
+      ]);
+      return resizedImage.div(tf.scalar(255.0)).expandDims();
+    });
+
+    const results = await model.predict(processedImages);
+
+    // @ts-expect-error hello
+    const classNameIndex = results.argMax(-1).dataSync()[0];
+    const classLabel = items[classNameIndex]?.name || 'Неизвестный класс';
+
+    return classLabel;
+  }
+
+  @Get('models')
+  async getModels() {
+    const ids = await readdir('./models');
+    const models: { id: string; name: string; type: 'photo' | 'video' }[] =
+      ids.map((id) => {
+        const { name } = JSON.parse(
+          readFileSync(`./models/${id}/meta.json`, {
+            encoding: 'utf-8',
+          }),
+        );
+
+        return {
+          id,
+          name,
+          type: 'photo',
+        };
+      });
+
+    return models;
+  }
+
   @Post('model/train')
   @FormDataRequest()
   async train(
     @Body()
-    { epochs, item1Images, item1Name, item2Images, item2Name }: TrainDto,
+    {
+      epochs,
+      modelName,
+      item1Name,
+      item2Name,
+      modelDescription,
+      item1Images,
+      item2Images,
+    }: TrainDto,
   ) {
     const item1ProcessedImages = item1Images.map((file) => {
       const decodedImage = tf.node.decodeImage(file.buffer);
@@ -52,17 +120,32 @@ export class AppController {
     );
 
     const BATCH_SIZE = 8;
-    const NUM_EPOCHS = 10;
 
     await model.fit(tf.concat(images), labels, {
       batchSize: BATCH_SIZE,
-      epochs: NUM_EPOCHS,
+      epochs: Number(epochs),
       shuffle: true,
-      callbacks: {
-        onEpochEnd: (epoch, logs) => {
-          console.log({ epoch, logs });
-        },
-      },
     });
+
+    const modelId = Date.now().toString();
+
+    const modelFolderPath = `./models/${modelId}`;
+
+    const meta = JSON.stringify({
+      name: modelName,
+      description: modelDescription,
+    });
+
+    const classNames = JSON.stringify([
+      {
+        name: item1Name,
+      },
+      { name: item2Name },
+    ]);
+
+    await mkdir(modelFolderPath, { recursive: true });
+    await writeFile(`${modelFolderPath}/meta.json`, meta);
+    await writeFile(`${modelFolderPath}/items.json`, classNames);
+    model.save(`file://${modelFolderPath}`);
   }
 }
