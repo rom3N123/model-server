149c8964027a5760981749b409669aa4a1abf81e
Roman Martynov <r.martynov@gk-adept.ru>
2023-10-08 21:10:29 +0300

    feat: add docker

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,15 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { NestjsFormDataModule } from 'nestjs-form-data';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [NestjsFormDataModule],
+  imports: [
+    ConfigModule.forRoot({
+      envFilePath: `.${process.env.NODE_ENV}.env`,
+    }),
+    NestjsFormDataModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,9 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  const PORT = process.env.PORT || 5000;
   const app = await NestFactory.create(AppModule);
   app.enableCors();
-  await app.listen(4001);
+  await app.listen(PORT, () => console.log(`Server started on port = ${PORT}`));
 }
 bootstrap();
